c4c623a3dfe96eb4bd3173f258954f0b1fc2b36d
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2024-06-28 14:27:04 -0400

    Fix reading/writing and add config code

--- src/data/data_manager.rs
+++ src/data/data_manager.rs
@@ -1,10 +1,17 @@
-use crate::data::wireguard_data::WireGuardData;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
+use std::io;
 use std::io::{Read, Write};
 
+use crate::data::wireguard_data::WireGuardData;
+
 pub fn read_json_file() -> Result<WireGuardData, Box<dyn Error>> {
-    let mut file = File::create_new("data.json")?;
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true) // required to set truncate to false
+        .truncate(false)
+        .create(true)
+        .open("data.json")?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
 
@@ -13,8 +20,15 @@ pub fn read_json_file() -> Result<WireGuardData, Box<dyn Error>> {
 }
 
 pub fn save_json_file(data: &WireGuardData) -> Result<(), Box<dyn Error>> {
-    let mut file = File::create_new("data.json")?;
+    let mut file = File::create("data.json")?;
     let json = serde_json::to_string_pretty(data)?;
     file.write_all(json.as_bytes())?;
+    Ok(())
+}
+
+pub fn save_wireguard_config(data: &WireGuardData, file_path: &String) -> Result<(), io::Error> {
+    let mut file = File::create(file_path)?;
+    let config = data.get_server_config();
+    file.write_all(config.as_bytes())?;
     Ok(())
 }
