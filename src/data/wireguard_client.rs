10569ee978faa82b410e0c2270e6b599464d1fa8
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2024-06-28 13:50:22 -0400

    Create client config code

--- src/data/wireguard_client.rs
+++ src/data/wireguard_client.rs
@@ -15,3 +15,37 @@ pub struct WireGuardClientData {
     pub client_allowed_ips: Vec<String>,
     pub dns: Option<String>,
 }
+
+impl WireGuardClientData {
+    pub fn get_server_config(&self) -> String {
+        let mut result = format!("# Name: {}", self.name);
+        result += "\n[Peer]";
+        result += &format!("\nPublicKey = {}", self.public_key);
+        if let Some(preshared_key) = &self.preshared_key {
+            result += &format!("\nPresharedKey = {preshared_key}");
+        }
+        result += &format!("\nAllowedIPs = {}", self.server_allowed_ips.join(","));
+        if let Some(persistent_keep_alive) = self.persistent_keep_alive {
+            result += &format!("\nPersistentKeepAlive = {persistent_keep_alive}");
+        }
+        result
+    }
+
+    pub fn get_client_config(&self, server_public_key: &String, server_endpoint: &String) -> String {
+        let mut result = format!("# Name: {}", self.name);
+        result += "\n[Interface]";
+        result += &format!("\nPrivateKey = {}", self.private_key);
+        result += &format!("\nAddress = {}", self.address);
+        if let Some(dns) = &self.dns {
+            result += &format!("\nDNS = {dns}");
+        }
+        result += "\n\n[Peer]";
+        result += &format!("\nPublicKey = {server_public_key}");
+        if let Some(preshared_key) = &self.preshared_key {
+            result += &format!("\nPresharedKey = {preshared_key}");
+        }
+        result += &format!("\nAllowedIPs = {}", self.client_allowed_ips.join(","));
+        result += &format!("\nEndpoint = {server_endpoint}");
+        result
+    }
+}
