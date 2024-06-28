d7dbdb3ddfc10b6d49bd357705a329e6429cff01
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2024-06-28 12:30:19 -0400

    setup test server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::thread;
+
+mod server;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    server::start_server().await;
+    
+    // add something else later?
+    
+    loop {
+        thread::park();
+    }
 }
---
+++ src/server.rs
@@ -0,0 +1,38 @@
+use std::net::SocketAddr;
+
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Router;
+use tokio::net::TcpListener;
+
+pub async fn start_server() {
+    let address = SocketAddr::from(([0, 0, 0, 0], 6252));
+    tokio::spawn(async move {
+        #[allow(unused_variables)] // bugged
+        let listener = match TcpListener::bind(address).await {
+            Ok(listener) => listener,
+            Err(error) => {
+                panic!("Could not bind to address {address}: {error}");
+            }
+        };
+        let server = axum::serve(
+            listener,
+            Router::new()
+                .route("/test", axum::routing::get(test_function))
+                .into_make_service_with_connect_info::<SocketAddr>(),
+        );
+        server.await.unwrap();
+        panic!("Server stopped unexpectedly");
+    });
+
+    println!("Server started on {}", address);
+}
+
+async fn test_function(
+) -> impl IntoResponse {
+    println!("Got request");
+    (
+        StatusCode::OK,
+        serde_json::to_string(&"Test response").unwrap(),
+    )
+}
