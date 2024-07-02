aa46624d9f5d2545f42e6d695b67b12ce9f19609
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2024-07-02 14:30:52 -0400

    Get default network interface

--- src/data/config.rs
+++ src/data/config.rs
@@ -1,13 +1,32 @@
+use netdev::Interface;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AppConfig {
     #[serde(default = "default_interface")]
     pub interface: String,
+    #[serde(default = "default_network_interface")]
+    pub network_interface: String,
     #[serde(default = "default_address")]
     pub address: String,
     #[serde(default = "default_wireguard_config_path")]
     pub wireguard_config_path: String,
+}
+
+impl AppConfig {
+    pub fn get_network_interface_name(&self) -> String {
+        if !self.network_interface.is_empty() {
+            return self.network_interface.to_owned();
+        }
+        netdev::interface::get_default_interface().unwrap().name
+    }
+    
+    pub fn get_network_interface(&self) -> Option<Interface> {
+        let interface_name = self.get_network_interface_name();
+        netdev::interface::get_interfaces().iter().find(|interface| {
+            interface.name == interface_name
+        }).map(Interface::to_owned)
+    }
 }
 
 fn default_interface() -> String {
@@ -16,6 +35,10 @@ fn default_interface() -> String {
     } else {
         "utun3".into()
     }
+}
+
+fn default_network_interface() -> String {
+    "".to_string()
 }
 
 fn default_address() -> String {
